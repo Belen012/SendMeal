c9ea64fd66cc7fed317efa8d8193ca87189aae7f
Belen012 <belu_eceiza@outlook.es>
2020-09-04 22:13:22 -0300

    lista y boton

--- app/src/main/java/ar/com/giancarellieceiza/sendmeal/MainActivity.java
+++ app/src/main/java/ar/com/giancarellieceiza/sendmeal/MainActivity.java
@@ -3,12 +3,35 @@ package ar.com.giancarellieceiza.sendmeal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> items = new ArrayList<String>(Arrays.asList(new String[]{"manzanas"}));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView list = findViewById(R.id.list);
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        list.setAdapter(itemsAdapter);
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                items.add("perlas");
+            }
+        });
+
     }
 }
